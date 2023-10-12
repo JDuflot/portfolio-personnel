0b9b58ff551f276d14ddb75d411ddeb481d4a58a
JDuflot <124251449+JDuflot@users.noreply.github.com>
2023-10-12 12:41:16 +0200

    final

---
+++ script.js
@@ -0,0 +1,102 @@
+// menu burger toggle
+let menuIcon = document.querySelector(".menu-icon");
+let navlist = document.querySelector(".navlist")
+menuIcon.addEventListener("click",()=>{
+    menuIcon.classList.toggle("active");
+    navlist.classList.toggle("active");
+    document.body.classList.toggle("open");
+});
+
+// remove navlist
+navlist.addEventListener("click",()=>{
+    navlist.classList.remove("active");
+    menuIcon.classList.remove("active");
+    document.body.classList.remove("open");
+});
+
+// portfolio filter with mixitup 
+
+var mixer = mixitup('.skills-gallery',{
+    selectors: {
+        target: '.skills-box'
+    },
+    animation: {
+        duration: 500
+    }
+});
+
+// Initialize swiper js 
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    autoplay:{
+        delay:3000,
+        disableOnInteraction:false,
+    },
+
+    breakpoints: {
+        576:{
+            slidesPerView:2,
+            spaceBetween:10,
+        },
+        1200:{
+            slidesPerView:3,
+            spaceBetween:20,
+        },
+    }
+  });
+
+// side progress bar 
+
+let calcScrollValue = ()=>{
+    let scrollProgress = document.getElementById("progress");
+    let pos = document.documentElement.scrollTop;
+
+    let calcHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    let scrollValue = Math.round((pos * 100)/calcHeight);
+    
+    if(pos > 100){
+        scrollProgress.style.display = "grid";
+    }else{
+        scrollProgress.style.display = "none";
+    }
+
+    scrollProgress.addEventListener("click",()=>{
+        document.documentElement.scrollTop = 0;
+    });
+
+    scrollProgress.style.background = `conic-gradient(#fff ${scrollValue}%,#5EC4BC ${scrollValue}%)`;
+};
+
+window.onscroll = calcScrollValue;
+window.onload = calcScrollValue;
+
+
+// active menu 
+
+let menuLi = document.querySelectorAll("header ul li a");
+let section = document.querySelectorAll('section');
+
+function activeMenu() {
+    const currentPosition = window.scrollY + 97;
+    
+    // Parcourir les sections pour trouver la première section visible
+    let activeSectionIndex = -1;
+    section.forEach((sec, index) => {
+        if (currentPosition >= sec.offsetTop) {
+            activeSectionIndex = index;
+        }
+    });
+    // Ajouter la classe "active" à l'élément de menu correspondant
+    menuLi.forEach((sec) => sec.classList.remove("active"));
+    if (activeSectionIndex >= 0) {
+        menuLi[activeSectionIndex].classList.add("active");
+    }
+}
+activeMenu();
+window.addEventListener("scroll",activeMenu);
